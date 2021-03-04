c7c24bf24395fa8bf0106b2d0da11eec29e94f94
ide-k <siddharthkhanna2k@gmail.com>
2021-03-03 19:59:50 -0500

    my attempt at css failed

--- src/Java/Main.java
+++ src/Java/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../resources/view/Login.fxml"));
-        primaryStage.setTitle("Athleticrate");
+        primaryStage.setTitle("AthletiCrate");
         primaryStage.setScene(new Scene(root, 335, 600));
         primaryStage.show();
 
